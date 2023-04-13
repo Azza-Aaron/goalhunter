80e1b63cfb03cdfab65010dccc76a1e69c07eca6
azz_aaron <aaronbwinfield@gmail.com>
2023-04-13 18:32:56 +0800

    database to env for heroku

--- back_end/src/data_base/index.js
+++ back_end/src/data_base/index.js
@@ -3,13 +3,13 @@ const {checkLoggedInTable} = require("./init.js")
 const session = require("express-session");
 const pgSession = require('connect-pg-simple')(session)
 
-const config = {
+const config = process.env.NODE_ENV === 'dev' ? {
   user: process.env.DB_USER ?? "postgres",
   host: process.env.DB_HOST ?? "localhost",
   database: process.env.DB ?? "postgres",
   password: process.env.DB_PASSWORD ?? "password",
   port: process.env.DB_PORT ?? 5432,
-}
+} : {connectionString: process.env.DATABASE_URL}
 
 const client = new Client(config)
 client.connect();
